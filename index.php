f2eb25441c9e0763eee7c7e5da179af7db324821
cewart38 <95366271+cewart38@users.noreply.github.com>
2023-04-14 17:29:20 +0100

    set up web service

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "Hello World!";
+// Replace this with your custom web service endpoint
+$webServiceEndpoint = 'https://csmcewar-6131comp.azurewebsites.net/';
+
+// Send a GET request to the custom web service
+$response = file_get_contents($webServiceEndpoint);
+
+// Parse the response and extract the temperature and humidity values
+$data = json_decode($response, true);
+$temperature = $data['temperature'];
+$humidity = $data['humidity'];
+
+// Display the temperature and humidity values on a webpage
+echo 'Temperature: ' . $temperature . '&deg;C<br>';
+echo 'Humidity: ' . $humidity . '%<br>';
+
+?>
