e339e994bbd0ad6d1a259efccba9f9951809061c
cewart38 <95366271+cewart38@users.noreply.github.com>
2023-04-14 20:06:10 +0100

    wfdef

--- data.php
+++ data.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     echo 'HELLO';
-    print_r("AHHH", $GET);
+    print_r($_GET);
     $temperature = $_GET['temp'];
     $humidity = $_GET['humidity'];
 
