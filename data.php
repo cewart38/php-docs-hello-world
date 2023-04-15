103e1d1cabe218a8ef449f68e170e706cd83c935
cewart38 <95366271+cewart38@users.noreply.github.com>
2023-04-15 12:48:26 +0100

    dfg

--- data.php
+++ data.php
@@ -1,14 +1,21 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $query = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
-    parse_str($query, $params);
 
-    $temperature = $params['temp'];
-    $humidity = $params['humidity'];
-
-    echo 'Temperature: ' . $temperature . ' °C<br>';
-    echo 'Humidity: ' . $humidity . ' %';
-} else {
-    echo 'No data received.';
+// Check if request method is POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Get the JSON data from the request body
+  $json_data = file_get_contents('php://input');
+  
+  // Decode the JSON data into an associative array
+  $data = json_decode($json_data, true);
+  
+  // Get the temperature and humidity values from the data array
+  $temperature = $data['temperature'];
+  $humidity = $data['humidity'];
+  
+  // Do something with the temperature and humidity values
+  // For example, save them to a database or log file
+  echo "Temperature: " . $temperature . "\n";
+  echo "Humidity: " . $humidity . "\n";
 }
+
 ?>
