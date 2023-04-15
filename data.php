edbeffbad6b3135a4e05d2e2490ecabd541fc310
cewart38 <95366271+cewart38@users.noreply.github.com>
2023-04-15 13:29:26 +0100

    wdqd

--- data.php
+++ data.php
@@ -1,10 +1,13 @@
 <?php
-		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            // Retrieve data from GET request
-            $temperature = $_GET['temp'];
-            $humidity = $_GET['humidity'];
-            
-            // Echo data to webpage
-            echo "Temperature: " . $temperature . "<br>";
-            echo "Humidity: " . $humidity;
-            ?>
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // Retrieve data from GET request
+    $temperature = $_GET['temp'];
+    $humidity = $_GET['humidity'];
+
+    // Echo data to webpage
+    echo "Temperature: " . $temperature . "<br>";
+    echo "Humidity: " . $humidity;
+} else {
+    echo ("AHHHHH");
+}
+?>
