0abd3ddfe7de777781f59fcaf98bcf65af6c81f0
cewart38 <95366271+cewart38@users.noreply.github.com>
2023-04-15 13:11:50 +0100

    sdfsdf

--- data.php
+++ data.php
@@ -1,24 +1,12 @@
 <?php
-echo("Why wont you work");
-// Check if request method is POST
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  // Get the JSON data from the request body
-  echo ("POST data: ");
-  print_r($_POST);
-  echo ("PLease work");
-  $json_data = file_get_contents('php://input');
-  echo $json_data;
-  // Decode the JSON data into an associative array
-  $data = json_decode($json_data, true);
-  
-  // Get the temperature and humidity values from the data array
-  $temperature = $data['temperature'];
-  $humidity = $data['humidity'];
-  
-  // Do something with the temperature and humidity values
-  // For example, save them to a database or log file
-  echo "Temperature: " . $temperature . "\n";
-  echo "Humidity: " . $humidity . "\n";
-}
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$temperature = $_POST['temperature'];
+			$humidity = $_POST['humidity'];
 
-?>
+
+			echo '<p>temperature: ' . $temperature . '</p>';
+			echo '<p>humidity: ' . $humidity . '</p>';
+		} else {
+			echo '<p>No POST request received.</p>';
+		}
+	?>
