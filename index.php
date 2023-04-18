b7aa5f69cadc1439197a59c79eb3b35315537584
cewart38 <95366271+cewart38@users.noreply.github.com>
2023-04-18 20:05:03 +0100

    qwdwd

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
+// Check if POST request was received
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $temperature = $_POST['temperature'];
-    $humidity = $_POST['humidity'];
+  // Read the POST request payload
+  $payload = file_get_contents('php://input');
 
-    // Display the data
-    echo "<h2>Temperature and Humidity Reading:</h2>";
-    echo "<p>Temperature: $temperature</p>";
-    echo "<p>Humidity: $humidity</p>";
+  // Decode the JSON payload
+  $data = json_decode($payload, true);
+
+  // Display all the values
+  foreach ($data as $key => $value) {
+    echo "$key: $value <br>";
+  }
 } else {
-    echo "Not Receiving POST Request";
+  // Display an error message if no POST request was received
+  echo "Error: No POST request received";
 }
 ?>
