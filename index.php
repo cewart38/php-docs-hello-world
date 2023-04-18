3a53a52ff5655fd3abbf8d69b7c8ae8b0e19679a
cewart38 <95366271+cewart38@users.noreply.github.com>
2023-04-18 21:09:32 +0100

    sdfdf

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
-// Check if POST request was received
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  // Read the POST request payload
-  $payload = file_get_contents('php://input');
 
-  // Decode the JSON payload
-  $data = json_decode($payload, true);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the POST body and decode it as JSON
+    $data = json_decode(file_get_contents('php://input'), true);
 
-  // Display all the values
-  foreach ($data as $key => $value) {
-    echo "$key: $value <br>";
-  }
+    // Print the data received
+    foreach ($data as $value) {
+        echo $value . "<br>";
+    }
 } else {
-  // Display an error message if no POST request was received
-  echo "Error: No POST request received";
+    echo "No POST request received";
 }
+
 ?>
+
+
+
+
+
+
