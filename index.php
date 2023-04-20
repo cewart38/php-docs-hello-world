1bf9005d2a0834491be2abe8c4cd8b5155400325
cewart38 <95366271+cewart38@users.noreply.github.com>
2023-04-20 22:59:24 +0100

    rgrsd

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-$serverName = "localhost";
-$dBUserName = "root";
-$dBPassword = "";
-$dBName = "personalExpenses";
-
-$conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
-
-if (! $conn) {
-    die("Connection failed: " . mysql_connect_error());
-}
+phpinfo();
+?>
