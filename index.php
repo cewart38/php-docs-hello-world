3f20a26bf28d142b547b32136e51d4f602fa7d04
cewart38 <95366271+cewart38@users.noreply.github.com>
2023-04-20 18:05:03 +0100

    dfg

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
 // database connection parameters
 $servername = "localhost";
-$username = "yourusername";
-$password = "yourpassword";
-$dbname = "yourdbname";
+$username = "root";
+$dbname = "readings";
 
 // retrieve temperature and humidity readings from POST request
 $temperature = $_POST['temperature'];
 $humidity = $_POST['humidity'];
 
 // create database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, "", $dbname);
 
 // check for connection errors
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 // prepare SQL statement to insert data into table
@@ -22,9 +21,9 @@ $sql = "INSERT INTO readings (temperature, humidity) VALUES ('$temperature', '$h
 
 // execute SQL statement
 if ($conn->query($sql) === TRUE) {
-  echo "Data inserted successfully";
+    echo "Data inserted successfully";
 } else {
-  echo "Error inserting data: " . $conn->error;
+    echo "Error inserting data: " . $conn->error;
 }
 
 // close database connection
